6bd0bdf9d7d3be15ac3abea071d508396dc53c50
elouanjef <elouan.rabouin@insa-cvl.fr>
2022-12-08 14:56:50 +0100

    tache1

--- ciwFiles.py
+++ ciwFiles.py
@@ -30,3 +30,17 @@ N=ciw.create_network(
     number_of_servers=[1, 1, 1, 1]
 
 )
+
+average_waits = []
+simtime = 180
+
+for s in range(1000):
+    ciw.seed(s)
+    Q = ciw.Simulation(N)
+    Q.simulate_until_max_time(simtime)
+    recs = Q.get_all_records()
+    waits = [r.waiting_time for r in recs]
+    average_waits.append(sum(waits)/len(waits))
+
+mean_waiting_time = sum(average_waits)/len(average_waits)
+print("Temps moyen d'attente: ", mean_waiting_time)
