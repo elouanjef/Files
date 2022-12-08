f8ae80cffad4cab63e1ed575ed9f74c2a068da3d
elouanjef <elouan.rabouin@insa-cvl.fr>
2022-12-08 20:27:14 +0100

    Tache2 terminée, avec un indice de confiance de 2,5%

--- ciwFiles.py
+++ ciwFiles.py
@@ -33,9 +33,14 @@ N=ciw.create_network(
 
 )
 
-average_waits = {}
-simtime = 10
-nbexecs = 50
+
+
+average_waits_dict = {}
+average_waits = []
+simtime = 5
+nbexecs = 1
+
+
 
 def simu(time, nb):
     for i in range(nb):
@@ -45,25 +50,70 @@ def simu(time, nb):
         recs = Q.get_all_records()
         waits = [r.waiting_time for r in recs]
         mean_waits = sum(waits)/len(waits)
-        print(f"Temps moyen d'attente de la simulation n°{i}: {mean_waits}")
-        average_waits[f"{time}"] = mean_waits
+        print(f"Simulation n°{i}: {round(mean_waits, 6)} sec")
+        average_waits_dict[f"{time}"] = mean_waits
+        average_waits.append(mean_waits)
 
 # while nbexecs <= 600:
 #      simu(simtime, nbexecs)
 #      nbexecs+=10
 
-while simtime <= 100:
-     simu(simtime, nbexecs)
-     simtime+=10
+# while simtime <= 100:
+#      simu(simtime, nbexecs)
+#      simtime+=10
 
 
-print(average_waits)
 
-graph = average_waits.items()
-x, y = zip(*graph)
+# print(average_waits)
+#
+# graph = average_waits.items()
+# x, y = zip(*graph)
+#
+# plt.plot(x, y)
+# # plt.show()
 
-plt.plot(x, y)
-plt.show()
+I = []
+
+simu(simtime, 1000)
 
 mean_waiting_time = sum(average_waits)/len(average_waits)
-print(f"Temps moyen d'attente: {mean_waiting_time}")
+print(f"Temps moyen d'attente: {round(mean_waiting_time, 6)} sec")
+
+prct = 2.5
+confiance = mean_waiting_time*(prct/100)
+gauche = round(mean_waiting_time - confiance, 6)
+droite = round(mean_waiting_time + confiance, 6)
+I = [gauche, droite]
+
+print(I)
+
+# average_waits_dict = {}
+average_waits = []
+# simtime = 10
+nbexecs = 5
+
+cpt = 0
+while True:
+    simu(simtime, nbexecs)
+    mean_waiting_time = sum(average_waits)/len(average_waits)
+    print(f"Moyenne: {mean_waiting_time}")
+    if I[0] < mean_waiting_time < I[1]: break
+    nbexecs+=5
+print(f"A partir d'un temps de {nbexecs}, car il a une moyenne de séjour de {round(mean_waiting_time, 6)} sec et I = [{gauche} ; {droite}]")
+
+##------------------------##
+##---------Tache1---------##
+# On a réalisé 2 graphes différents:
+# - l'un où on visualise que 50 itérations de l'algorithme permettant de trouver le temps moyen de séjour, suffisent largement.
+# En effet, on découvre, qu'on fasse 50 ou 500 itérations, le temps moyen restera toujours du même ordre de grandeur.
+# Donc pour une question d'optimisation, le graphe sert à confirmer qu'on peut faire seulement 50 itérations.
+# - l'autre graphe montre que le temps moyen de séjour est proportionnel au temps de la simulation.
+
+##------------------------##
+##---------Tache2---------##
+# Pour cette tâche, il faut établir un intervalle de confiance, si on prend par exemple 5%, on doit vérifier à partir de quel nombre de répitions de simulations, le temps moyen de séjour se situe dans cet intervalle.
+# Pour ceci, on fait une 1000 simulations, d'un temps quelconque, ces 1000 simulations corresponderont à la valeur réelle.
+# A partir de ces 1000 simulations, on établit un intervalle de confiance:
+# I = [tps_moyen_sjr_reel - 5%; tps_moyen_sjr_reel + 5%]
+#
+# On vérifie ensuite à partir de quel nombre de simulations, on a tps_moyen_sjr_theoX appartient à I (à partir de quel indice)
